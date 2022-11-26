031688c7e8ef067a1755a8ea3c416c9957cfdcd2
mdsahhinreza <mdsahhinreza@gmail.com>
2022-11-27 02:24:23 +0600

    Firebase And Context added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSMYUlWoq3d89CzU0lZkeWvXx7WPKZDyw",
+  authDomain: "funta-phone.firebaseapp.com",
+  projectId: "funta-phone",
+  storageBucket: "funta-phone.appspot.com",
+  messagingSenderId: "99248754948",
+  appId: "1:99248754948:web:d3d506f8db8d756b780051",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
